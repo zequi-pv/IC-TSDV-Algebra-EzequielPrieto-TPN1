ea2c7c6363fb7f57a68619e014ac7d635afa7f68
Nicodragon25 <nicolasariellaure@gmail.com>
2023-08-14 12:54:39 -0300

    Add DrawScript

--- Assets/Scripts/DrawingScript.cs
+++ Assets/Scripts/DrawingScript.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class DrawingScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] LineRenderer line;
+
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (line.positionCount == 1)
+                line.SetPosition(0, new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+            else
+                line.SetPosition(line.positionCount - 1, new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+
+            line.positionCount += 1;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            line.positionCount += 1;
+            line.SetPosition(line.positionCount - 1, line.GetPosition(0));
+        }
     }
 }
