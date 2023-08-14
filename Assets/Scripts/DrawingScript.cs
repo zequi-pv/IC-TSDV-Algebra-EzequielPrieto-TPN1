98f6018b8130eeed143c9478445dab3a99b85c1f
Nicodragon25 <nicolasariellaure@gmail.com>
2023-08-14 13:08:58 -0300

    Update DrawingScript

--- Assets/Scripts/DrawingScript.cs
+++ Assets/Scripts/DrawingScript.cs
@@ -7,18 +7,30 @@ public class DrawingScript : MonoBehaviour
 
     [SerializeField] LineRenderer line;
 
+    Vector3 worldPosition;
+    Vector3 mousePosition;
+
+    private void Start()
+    {
+        line.loop = false;
+    }
     void Update()
     {
+
         if (Input.GetMouseButtonDown(0))
         {
-            if (line.positionCount == 1)
-                line.SetPosition(0, new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
-            else
-                line.SetPosition(line.positionCount - 1, new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+            mousePosition = Input.mousePosition;
+            worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-            line.positionCount += 1;
+            if (line.positionCount == 1 && line.GetPosition(0) == new Vector3(0, 0, 0))
+                line.SetPosition(0, new Vector3(worldPosition.x, worldPosition.y, 1));
+            else
+            {
+                line.positionCount += 1;
+                line.SetPosition(line.positionCount - 1, new Vector3(worldPosition.x, worldPosition.y, 1));
+            }
         }
-        if (Input.GetMouseButtonDown(1))
+        else if (Input.GetMouseButtonDown(1))
         {
             line.positionCount += 1;
             line.SetPosition(line.positionCount - 1, line.GetPosition(0));
