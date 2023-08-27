81e785517f6719bfa9f1b23090ad74d51a74bcb5
Nicodragon25 <nicolasariellaure@gmail.com>
2023-08-27 20:35:15 -0300

    Update CollisionDetection

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -15,10 +15,15 @@ public class BallController : MonoBehaviour
         transform.Translate(transform.up * y * speed * Time.deltaTime);
         transform.Translate(transform.right * x * speed * Time.deltaTime);
 
-        CheckInside();
+        int linesRight = CheckInside();
+
+        if (linesRight % 2 != 0 && linesRight != 0)
+            GetComponent<SpriteRenderer>().color = Color.green;
+        else
+            GetComponent<SpriteRenderer>().color = Color.white;
     }
 
-    void CheckInside()
+    void CheckInsideRaycast()
     {
         List<RaycastHit2D> raycastHit2Ds = new List<RaycastHit2D>();
 
@@ -42,10 +47,54 @@ public class BallController : MonoBehaviour
                 break;
         }
 
-        if (raycastHit2Ds.Count % 2 != 0)
+        if (raycastHit2Ds.Count % 2 != 0 && raycastHit2Ds.Count != 0)
             GetComponent<SpriteRenderer>().color = Color.green;
         else
             GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    int CheckInside()
+    {
+        int collisionsCount = 0;
+        foreach (LineRenderer line in FindObjectsOfType<LineRenderer>())
+        {
+            for (int i = 0; i < line.positionCount; i++)
+            {
+                bool isRight = false;
+                Vector2 upperPointPos;
+                Vector2 lowerPointPos;
+
+                if (i == line.positionCount - 1)
+                {
+                    if (line.GetPosition(i).y < line.GetPosition(0).y)
+                    {
+                        lowerPointPos = line.GetPosition(i);
+                        upperPointPos = line.GetPosition(0);
+                    }
+                    else
+                    {
+                        lowerPointPos = line.GetPosition(0);
+                        upperPointPos = line.GetPosition(i);
+                    }
+                }
+                else
+                {
+                    if (line.GetPosition(i).y < line.GetPosition(i + 1).y)
+                    {
+                        lowerPointPos = line.GetPosition(i);
+                        upperPointPos = line.GetPosition(i + 1);
+                    }
+                    else
+                    {
+                        lowerPointPos = line.GetPosition(i + 1);
+                        upperPointPos = line.GetPosition(i);
+                    }
+                }
+                isRight = 0 >= (transform.position.x - upperPointPos.x) * (lowerPointPos.y - upperPointPos.y) - (transform.position.y - upperPointPos.y) * (lowerPointPos.x - upperPointPos.x);
+                if (isRight && transform.position.y > lowerPointPos.y && transform.position.y < upperPointPos.y) collisionsCount++;
+            }
+        }
+        return collisionsCount;
     }
 
 }
